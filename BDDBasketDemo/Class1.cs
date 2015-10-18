318d5ab8adb99e8bfb9cd24f6cbace87c9debf28
andrew.jack999 <andrew.jack@euromoneyplc.com>
2015-10-18 22:47:53 +0100

    First commit

---
+++ BDDBasketDemo/Class1.cs
@@ -0,0 +1,77 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace MyNamespace
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        Basket _basket;
+        Object _item;
+
+        [Given(@"I create a new basket")]
+        [When(@"I create a new basket")]
+        public void WhenICreateANewBasket()
+        {
+            _basket = new Basket();
+        }
+
+        [Then(@"The basket has no contents")]
+        public void ThenTheBasketHasNoContents()
+        {
+            Assert.IsEmpty(_basket.Contents);
+        }
+
+        [Given(@"I add an item to the basket")]
+        [When(@"I add an item to the basket")]
+        public void WhenIAddAnItemToTheBasket()
+        {
+            _item = new object();
+
+            _basket.Add(_item);
+        }
+
+        [Then(@"the basket contains the added item")]
+        public void ThenTheBasketContainsTheAddedItem()
+        {
+            Assert.IsNotEmpty(_basket.Contents);
+        }
+
+        [When(@"I remove an item from the basket")]
+        public void WhenIRemoveAnItemFromTheBasket()
+        {
+            
+
+            _basket.Remove(_item);
+        }
+
+        [Then(@"the basket does not contain the item")]
+        public void ThenTheBasketDoesNotContainTheItem()
+        {
+            Assert.IsEmpty(_basket.Contents);
+        }
+    }
+
+    public class Basket
+    {
+        public Basket()
+        {
+            Contents = new List<object>();
+        }
+        public List<object> Contents { get; set; }
+
+        internal void Add(object item)
+        {
+            Contents.Add(item);
+        }
+
+        internal void Remove(object item)
+        {
+            Contents.Remove(item);
+        }
+    }
+}
+
